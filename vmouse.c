eb4186fe6fc9f38c36bdf8f9dd66b2587e02ae54
Idan <speidy@gmail.com>
2018-02-25 08:04:59 +0200

    vmouse: handle udp requests and process them

--- vmouse.c
+++ vmouse.c
@@ -13,7 +13,7 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Guy B");
 MODULE_DESCRIPTION("A virtual mouse device");
 
-#define PORT 2325
+#define PORT 1337
 
 struct input_dev *mouse_dev;
 
@@ -42,8 +42,10 @@ int recv_msg(struct socket *sock, struct sockaddr_in *cl,
 
 	size = kernel_recvmsg(sock, &msg, &iov, 1, len, msg.msg_flags);
 
-	if (size > 0)
-		printk(KERN_ALERT "the message is : %s\n",buf);
+	// if (size > 0)
+	// {
+	// 	printk(KERN_ALERT "the message is : %s\n",buf);
+	// }
 
 	return size;
 }
@@ -66,9 +68,54 @@ int send_msg(struct socket *sock, struct sockaddr_in *cl, char *buf, int len)
 	size = kernel_sendmsg(sock, &msg, &iov, 1, len);
 
 	if (size > 0)
+	{
 		printk(KERN_INFO "message sent!\n");
+	}
 
 	return size;
+}
+
+static int handle_mouse_movements(unsigned char *buf, const size_t len)
+{
+	pr_info("got mouse request\n");
+
+	typedef struct
+	{
+		char type;
+		union
+		{
+			int rel_x;
+			int rel_y;
+		};
+
+		union
+		{
+			int btn;
+			bool on;
+		};
+	} __packed MouseRequest;
+
+	MouseRequest *req = buf;
+
+	pr_info("type %c rel_x %d rel_y %d\n", req->type, req->rel_x, req->rel_y);
+
+	switch(req->type)
+	{
+	case 'M': // movement
+		input_report_rel(mouse_dev, REL_X, req->rel_x);
+		input_report_rel(mouse_dev, REL_X, req->rel_y);
+		break;
+
+	case 'C': // click
+		// input_report_key(mouse_dev, )
+
+	default:
+		pr_warn("got unknown type: %c\n", req->type);
+	}
+
+	input_sync(mouse_dev);
+
+	return 0;
 }
 
 int start_listen(void)
@@ -103,54 +150,31 @@ int start_listen(void)
 		memset(&buf, 0, len+1);
 		memset(&client, 0, sizeof(struct sockaddr_in));
 		size = recv_msg(svc->listen_socket, &client, buf, len);
-		if (size < 0) {
+		if (size < 0) 
+		{
 			return -1;
 		}
-		send_msg(svc->listen_socket, &client, buf, size);
+		handle_mouse_movements(buf, len);
+		// send_msg(svc->listen_socket, &client, buf, size);
 	}
 
 	return 0;
 }
 
 
-static int do_movements_test(void *cb_data)
-{
-	pr_info("moving mouse\n");
-
-	int x = 0;
-	while(1)
-	{
-		input_report_rel(mouse_dev, REL_X, x);
-		input_report_rel(mouse_dev, REL_Y, x);
-		input_report_key(mouse_dev, BTN_RIGHT, 1);
-		input_report_key(mouse_dev, BTN_RIGHT, 0);
-		input_sync(mouse_dev);
-		msleep(1000);
-		x += 1;
-
-		if (x == 10)
-		{
-			break;
-		}
-	}
-
-	pr_info("done\n");
-
-	return 0;
-}
 
 
 static int __init vmouse_init(void)
 {
-    pr_info("Hello from Phone vMouse!\n");
+	pr_info("Hello from Phone vMouse!\n");
 
-    mouse_dev = input_allocate_device();
-    if (NULL == mouse_dev)
-    {
-    	return -ENOMEM;
-    }
+	mouse_dev = input_allocate_device();
+	if (NULL == mouse_dev)
+	{
+		return -ENOMEM;
+	}
 
-    mouse_dev->name = "Phone vMouse";
+	mouse_dev->name = "Phone vMouse";
 
 	input_set_capability(mouse_dev, EV_REL, REL_X);
 	input_set_capability(mouse_dev, EV_REL, REL_Y);
@@ -158,34 +182,34 @@ static int __init vmouse_init(void)
 	input_set_capability(mouse_dev, EV_KEY, BTN_RIGHT);
 	input_set_capability(mouse_dev, EV_KEY, BTN_MIDDLE);
 
-    if (input_register_device(mouse_dev))
-    {
-    	input_free_device(mouse_dev);
-    	return -ENOMEM;
-    }
+	if (input_register_device(mouse_dev))
+	{
+		input_free_device(mouse_dev);
+		return -ENOMEM;
+	}
 
-    // TODO: remove later
-    // kthread_run(do_movements_test, NULL, "movments_test");
+	// TODO: remove later
+	// kthread_run(do_movements_test, NULL, "movments_test");
 
 	pr_info("init mouse udp server\n");
-    svc = kmalloc(sizeof(struct service), GFP_KERNEL);
+	svc = kmalloc(sizeof(struct service), GFP_KERNEL);
 	svc->thread = kthread_run((void *)start_listen, NULL, "mouse-server");
-
 
     return 0;
 }
 
 static void __exit vmouse_cleanup(void)
 {
-    pr_info("killing udp server.\n");
-	if (svc->listen_socket != NULL) {
+	pr_info("killing udp server.\n");
+	if (svc->listen_socket != NULL) 
+	{
 		svc->listen_socket->ops->release(svc->listen_socket);
 		printk(KERN_ALERT "release socket\n");
 	}
 	kfree(svc);
 
 	pr_info("unregistering input device\n");
-    input_unregister_device(mouse_dev);
+	input_unregister_device(mouse_dev);
 	input_free_device(mouse_dev);
 }
 
