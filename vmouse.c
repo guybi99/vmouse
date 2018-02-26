122e8638f591e8dbc1946e96a51bc706a57bd5c4
Idan <speidy@gmail.com>
2018-02-26 22:52:21 +0200

    vmouse: tabs to spaces

--- vmouse.c
+++ vmouse.c
@@ -18,156 +18,156 @@ MODULE_DESCRIPTION("A virtual mouse device");
 struct input_dev *mouse_dev;
 
 struct service {
-	struct socket *listen_socket;
-	struct task_struct *thread;
+    struct socket *listen_socket;
+    struct task_struct *thread;
 };
 
 struct service *svc;
 
 int recv_msg(struct socket *sock, struct sockaddr_in *cl,
-		unsigned char *buf, int len) 
+        unsigned char *buf, int len) 
 {
-	struct msghdr msg;
-	struct kvec iov;
-	int size = 0;
+    struct msghdr msg;
+    struct kvec iov;
+    int size = 0;
 
-	iov.iov_base = buf;
-	iov.iov_len = len;
+    iov.iov_base = buf;
+    iov.iov_len = len;
 
-	msg.msg_control = NULL;
-	msg.msg_controllen = 0;
-	msg.msg_flags = 0;
-	msg.msg_name = cl;
-	msg.msg_namelen = sizeof(struct sockaddr_in);
+    msg.msg_control = NULL;
+    msg.msg_controllen = 0;
+    msg.msg_flags = 0;
+    msg.msg_name = cl;
+    msg.msg_namelen = sizeof(struct sockaddr_in);
 
-	size = kernel_recvmsg(sock, &msg, &iov, 1, len, msg.msg_flags);
+    size = kernel_recvmsg(sock, &msg, &iov, 1, len, msg.msg_flags);
 
-	// if (size > 0)
-	// {
-	// 	printk(KERN_ALERT "the message is : %s\n",buf);
-	// }
+    // if (size > 0)
+    // {
+    //  printk(KERN_ALERT "the message is : %s\n",buf);
+    // }
 
-	return size;
+    return size;
 }
 
 int send_msg(struct socket *sock, struct sockaddr_in *cl, char *buf, int len) 
 {
-	struct msghdr msg;
-	struct kvec iov;
-	int size;
+    struct msghdr msg;
+    struct kvec iov;
+    int size;
 
-	iov.iov_base = buf;
-	iov.iov_len = len;
+    iov.iov_base = buf;
+    iov.iov_len = len;
 
-	msg.msg_control = NULL;
-	msg.msg_controllen = 0;
-	msg.msg_flags = 0;
-	msg.msg_name = cl;
-	msg.msg_namelen = sizeof(struct sockaddr_in);
+    msg.msg_control = NULL;
+    msg.msg_controllen = 0;
+    msg.msg_flags = 0;
+    msg.msg_name = cl;
+    msg.msg_namelen = sizeof(struct sockaddr_in);
 
-	size = kernel_sendmsg(sock, &msg, &iov, 1, len);
+    size = kernel_sendmsg(sock, &msg, &iov, 1, len);
 
-	if (size > 0)
-	{
-		printk(KERN_INFO "message sent!\n");
-	}
+    if (size > 0)
+    {
+        printk(KERN_INFO "message sent!\n");
+    }
 
-	return size;
+    return size;
 }
 
 static void handle_mouse_event(unsigned char *buf, const size_t len)
 {
-	pr_info("got mouse request\n");
+    pr_info("got mouse request\n");
 
-	typedef struct
-	{
-		char type;
+    typedef struct
+    {
+        char type;
 
-		union
-		{
-			// type == movement
-			struct 
-			{
-				int rel_x;
-				int rel_y;
-			};
+        union
+        {
+            // type == movement
+            struct 
+            {
+                int rel_x;
+                int rel_y;
+            };
 
-			// type == click
-			struct
-			{
-				short btn;
-				char on;
-			};
-		};
+            // type == click
+            struct
+            {
+                short btn;
+                char on;
+            };
+        };
 
-	} __packed MouseRequest;
+    } __packed MouseRequest;
 
-	MouseRequest *req = buf;
+    MouseRequest *req = buf;
 
-	switch(req->type)
-	{
-	case 'M': // movemernt
-		pr_info("type %c rel_x %d rel_y %d\n", req->type, req->rel_x, req->rel_y);
-		input_report_rel(mouse_dev, REL_X, req->rel_x);
-		input_report_rel(mouse_dev, REL_X, req->rel_y);
-		break;
+    switch(req->type)
+    {
+    case 'M': // movemernt
+        pr_info("type %c rel_x %d rel_y %d\n", req->type, req->rel_x, req->rel_y);
+        input_report_rel(mouse_dev, REL_X, req->rel_x);
+        input_report_rel(mouse_dev, REL_X, req->rel_y);
+        break;
 
-	case 'C': // click
-		pr_info("type %c btn %x on %d\n", req->type, req->btn, req->on);
-		if(req->btn == BTN_LEFT || req->btn == BTN_RIGHT)
-		{
-			input_report_key(mouse_dev, req->btn, req->on);
-		}
-		break;
+    case 'C': // click
+        pr_info("type %c btn %x on %d\n", req->type, req->btn, req->on);
+        if(req->btn == BTN_LEFT || req->btn == BTN_RIGHT)
+        {
+            input_report_key(mouse_dev, req->btn, req->on);
+        }
+        break;
 
-	default:
-		pr_warn("got unknown type: %c\n", req->type);
-	}
+    default:
+        pr_warn("got unknown type: %c\n", req->type);
+    }
 
-	input_sync(mouse_dev);
+    input_sync(mouse_dev);
 }
 
 int start_listen(void)
 {
-	int error, i, size;
-	struct sockaddr_in sin, client;
-	int len = 15;
-	unsigned char buf[len+1];
+    int error, i, size;
+    struct sockaddr_in sin, client;
+    int len = 15;
+    unsigned char buf[len+1];
 
-	error = sock_create(PF_INET, SOCK_DGRAM, IPPROTO_UDP, &svc->listen_socket);
-	if(error < 0)
-	{
-		printk(KERN_ERR "cannot create socket\n");
-		return -1;
-	}
+    error = sock_create(PF_INET, SOCK_DGRAM, IPPROTO_UDP, &svc->listen_socket);
+    if(error < 0)
+    {
+        printk(KERN_ERR "cannot create socket\n");
+        return -1;
+    }
 
-	sin.sin_addr.s_addr = htonl(INADDR_ANY);
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(PORT);
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(PORT);
 
-	error = kernel_bind(svc->listen_socket, (struct sockaddr*)&sin,
-			sizeof(sin));
-	if(error < 0) 
-	{
-		printk(KERN_ERR "cannot bind socket, error code: %d\n", error);
-		return -1;
-	}
+    error = kernel_bind(svc->listen_socket, (struct sockaddr*)&sin,
+            sizeof(sin));
+    if(error < 0) 
+    {
+        printk(KERN_ERR "cannot bind socket, error code: %d\n", error);
+        return -1;
+    }
 
-	i = 0;
-	while (1) 
-	{
-		memset(&buf, 0, len+1);
-		memset(&client, 0, sizeof(struct sockaddr_in));
-		size = recv_msg(svc->listen_socket, &client, buf, len);
-		if (size < 0) 
-		{
-			return -1;
-		}
-		handle_mouse_event(buf, len);
-		// send_msg(svc->listen_socket, &client, buf, size);
-	}
+    i = 0;
+    while (1) 
+    {
+        memset(&buf, 0, len+1);
+        memset(&client, 0, sizeof(struct sockaddr_in));
+        size = recv_msg(svc->listen_socket, &client, buf, len);
+        if (size < 0) 
+        {
+            return -1;
+        }
+        handle_mouse_event(buf, len);
+        // send_msg(svc->listen_socket, &client, buf, size);
+    }
 
-	return 0;
+    return 0;
 }
 
 
@@ -175,51 +175,51 @@ int start_listen(void)
 
 static int __init vmouse_init(void)
 {
-	pr_info("Hello from Phone vMouse!\n");
+    pr_info("Hello from Phone vMouse!\n");
 
-	mouse_dev = input_allocate_device();
-	if (NULL == mouse_dev)
-	{
-		return -ENOMEM;
-	}
+    mouse_dev = input_allocate_device();
+    if (NULL == mouse_dev)
+    {
+        return -ENOMEM;
+    }
 
-	mouse_dev->name = "Phone vMouse";
+    mouse_dev->name = "Phone vMouse";
 
-	input_set_capability(mouse_dev, EV_REL, REL_X);
-	input_set_capability(mouse_dev, EV_REL, REL_Y);
-	input_set_capability(mouse_dev, EV_KEY, BTN_LEFT);
-	input_set_capability(mouse_dev, EV_KEY, BTN_RIGHT);
-	input_set_capability(mouse_dev, EV_KEY, BTN_MIDDLE);
+    input_set_capability(mouse_dev, EV_REL, REL_X);
+    input_set_capability(mouse_dev, EV_REL, REL_Y);
+    input_set_capability(mouse_dev, EV_KEY, BTN_LEFT);
+    input_set_capability(mouse_dev, EV_KEY, BTN_RIGHT);
+    input_set_capability(mouse_dev, EV_KEY, BTN_MIDDLE);
 
-	if (input_register_device(mouse_dev))
-	{
-		input_free_device(mouse_dev);
-		return -ENOMEM;
-	}
+    if (input_register_device(mouse_dev))
+    {
+        input_free_device(mouse_dev);
+        return -ENOMEM;
+    }
 
-	// TODO: remove later
-	// kthread_run(do_movements_test, NULL, "movments_test");
+    // TODO: remove later
+    // kthread_run(do_movements_test, NULL, "movments_test");
 
-	pr_info("init mouse udp server\n");
-	svc = kmalloc(sizeof(struct service), GFP_KERNEL);
-	svc->thread = kthread_run((void *)start_listen, NULL, "mouse-server");
+    pr_info("init mouse udp server\n");
+    svc = kmalloc(sizeof(struct service), GFP_KERNEL);
+    svc->thread = kthread_run((void *)start_listen, NULL, "mouse-server");
 
     return 0;
 }
 
 static void __exit vmouse_cleanup(void)
 {
-	pr_info("killing udp server.\n");
-	if (svc->listen_socket != NULL) 
-	{
-		svc->listen_socket->ops->release(svc->listen_socket);
-		printk(KERN_ALERT "release socket\n");
-	}
-	kfree(svc);
+    pr_info("killing udp server.\n");
+    if (svc->listen_socket != NULL) 
+    {
+        svc->listen_socket->ops->release(svc->listen_socket);
+        printk(KERN_ALERT "release socket\n");
+    }
+    kfree(svc);
 
-	pr_info("unregistering input device\n");
-	input_unregister_device(mouse_dev);
-	input_free_device(mouse_dev);
+    pr_info("unregistering input device\n");
+    input_unregister_device(mouse_dev);
+    input_free_device(mouse_dev);
 }
 
 module_init(vmouse_init);
